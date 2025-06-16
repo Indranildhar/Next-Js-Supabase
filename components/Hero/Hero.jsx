b8d52f954d77e47b88e911195232fcb6f4cf9af4
Indranil Dhar <indranil.dhar.97@gmail.com>
2025-06-16 20:00:11 +0530

    hero section code modify

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <div className='min-h-screen flex flex-col md:flex-row bg-gray-900 text-white md:p-20'>
         <motion.div
-            className="flex-1 space-y-6 px-6 py-10 border border-white"
+            className="flex-1 space-y-6 px-6 py-10 "
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.8 }}
@@ -15,17 +15,17 @@ export default function Hero() {
         </motion.div>
 
         <motion.div
-          className="flex-1 mt-20 md:mt-0 flex justify-center border border-red-500 relative md:py-20"
+          className="flex-1 mt-20 md:mt-0 flex justify-center  relative md:py-20 border border-red-500"
           initial={{ opacity: 0, scale: 0.8 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.8, delay: 0.3 }}
         >
-          <div className='w-64 h-64 md:w-80 md:h-80 xl:w-[500px] xl:h-[500px] rounded-full bg-yellow-500 overflow-hidden relative ring-4 ring-white'>
+          <div className='size-64 md:size-80 xl:size-[600px] rounded-full bg-gray-400 overflow-hidden relative ring-4 ring-white'>
             
 
           </div>
 
-          <div className='w-[300px] h-[450px]  md:w-[512px] md:h-[720px] absolute  -translate-y-[10%] bottom-0 '>
+          <div className='w-[300px] h-[450px]  md:w-[512px] md:h-[720px] absolute   bottom-0 border border-red-500'>
             <Image
               src={'/image/hero.png'}
               fill
